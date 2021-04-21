237b39ac12b92f4cf2c95a6d171c3b50d5b72548
aaron_dalao <aaron.dalao@gmail.com>
2021-04-22 06:45:09 +0800

    added selection sort in sorting.py

--- sorting.py
+++ sorting.py
@@ -6,11 +6,48 @@
 #   5) how to do a insert and merge sort again ? 
 
 
-
-
 # import random module to use random
 import random as r
 import array as arr
+
+
+
+
+
+# function call to my sorting algos
+#        
+# selection sort 
+def selection_Sort(mL):
+    #check the length of the Array
+    for i in range(len(mL)):
+        #find the smallest value in the element inside the unsorted array
+        minElement = i
+        for j in range(i+1, len(mL)):
+            # substitute the value of the minIndex if element[0] is greater than element[1]. 
+            if mL[minElement] > mL[j]:
+                minElement = j
+        # swap the minimum element with the first index/element
+        mL[i], mL[minElement] = mL[minElement] , mL[i]
+        print(myList)
+    return mL
+
+
+def bubSort(myList):
+    return myList
+
+def inSort(myList):
+    return myList
+
+def merSort(myList):
+    return myList
+
+
+
+
+
+
+
+
 
 # setting up input range
 x = int(input("Enter the start number you want to randomly generate:"))
@@ -26,6 +63,36 @@ for i in range(z):
     n = r.randint(x,y)
     #if n not in myList: #trying to 
     myList.append(n)    
-       
 
-print(myList)
+# choose a selection of sorting algo
+print('''
+
+choose a what sorting algo u want to see
+1 = selection sort
+2 = bubblesort
+3 = insert sort
+4 = merge sort
+5 = exit
+
+
+''')
+
+chooseSort = input()
+
+if chooseSort == "1":
+    print(f"this your unsorted array: {myList}")
+    selection_Sort(myList)
+    print(f"result is: {myList}")
+elif chooseSort == "2":
+    bubSort(myList)
+    print(myList)
+elif chooseSort == "3":
+    inSort(myList)
+    print(myList)
+elif chooseSort == "4":
+    merSort(myList)
+    print(myList)
+else:
+    print("enter numbers from 1 to 5 only thanks.")
+    
+
---
+++ tuts/importingAFunction.py
@@ -0,0 +1,8 @@
+# tuts for importing functions
+
+# step 1: IMPORT THE FUNCTION
+
+import functions.py
+
+for i in range(10):
+    print(f"the square of {i} is {square(i)}")
